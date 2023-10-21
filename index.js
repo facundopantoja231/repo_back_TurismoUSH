84b0afaa660b8794db278acea34495de98f9d218
facundopantoja231 <facundopantoja1@gmail.com>
2023-10-21 19:37:20 -0300

    Conexion a BD MySql Conexion con axios al Front-End

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express()
+
+app.use(express.json());
+app.use(cors());
+
+
+const con = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "turismo_ush"
+})
+
+const port = 3001
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.post("/register", (req, res) => {
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const correoElectronico = req.body.correoElectronico;
+    const nombreUsuario = req.body.nombreUsuario;
+    const contraseña = req.body.contraseña;
+
+    con.query("INSERT INTO usuarios (nombre, apellido, correoElectronico, nombreUsuario, contraseña) VALUES (?, ?, ?, ?, ?)", [nombre, apellido, correoElectronico, nombreUsuario, contraseña], (err, result) => {
+        if(result) {
+            res.send(result);
+        }else{
+            res.send({message: "Ingresa los detalles adecuados!"})
+        }
+    })
+})
+
+
+app.post("/login", (req,res) => {
+    const nombreUsuario = req.body.nombreUsuario;
+    const contraseña = req.body.contraseña;
+
+    con.query("SELECT * FROM usuarios WHERE nombreUsuario = ? AND contraseña = ?", [nombreUsuario, contraseña], (err, result) => {
+        if(err){
+            req.setEncoding({err: err});
+        }else{
+            if(result.length > 0) {
+                res.send(result);
+            }else{
+                res.send({message: "Error en el usuario o contraseña!"})
+            }
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`On port ${port}`)
+})
